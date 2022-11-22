bdf1e0a25a00a381e94aaca3a71bb56b0dae36ea
Diana-Maria Duca <72136604+913-Duca-Diana@users.noreply.github.com>
2022-11-22 15:51:01 +0200

    Add files via upload

---
+++ lab2.m
@@ -0,0 +1,19 @@
+﻿
+n=input('Nr of trials: ');
+p=input('Probability of success: ');
+x=0:n;
+
+px=binopdf(x,n,p);
+plot(x,px,'+');
+hold on;
+xx=0:0.1:n;
+cx=binocdf(xx,n,p);
+plot(xx,cx,'-');
+p1=binopdf(0,3,0.5)
+fprintf('p(x=0)=%1.4f',p1)
+
+%C=rand(3,n)
+%printf("[%f,%f,%f]\n]",C)
+%D=C<0.5;
+%E=sum(D)
+%hist(E)
---
+++ lab3.m
@@ -0,0 +1,47 @@
+﻿option=input('opt=','s')
+a=input('Give a')
+b=input('Give b')
+
+switch option
+case 'normal'
+n=input('Give n:');
+m=input('Give m:');
+%a)
+p1=normcdf(0,n,m)
+p2=1-p1
+%b)
+p3=normcdf(1,n,m)-normcdf(-1,n,m)
+p4=1-p3
+%c)
+p4=norminv(a,n,m)
+%d
+p5=norminv(1-b,n,m)
+
+case 'student'
+n=input('Give n:')
+%a)
+p1=tcdf(0,n)
+p2=1-p1
+%b)
+p3=tcdf(1,n)-normcdf(-1,n)
+p4=1-p3
+%c)
+p4=tinv(a,n)
+%d
+p5=tinv(1-b,n)
+
+case 'fischer'
+n=input('Give n:')
+m=input('Give m:')
+%a)
+p1=fcdf(0,n,m)
+p2=1-p1
+%b)
+p3=fcdf(1,n,m)-fcdf(-1,n,m)
+p4=1-p3
+%c)
+p4=finv(a,n,m)
+%d
+p5=finv(1-b,n,m)
+
+end
